fb2a386fed05687828153841cff8e566f66990e6
Mahmud Ramazanov <zumetlebar@gamil.com>
2023-11-13 02:16:00 +0300

    slider js starts

--- src/js/index.js
+++ src/js/index.js
@@ -31,6 +31,69 @@ langOptions.forEach((el) => {
   });
 });
 // languages finished
+// slider starts
+const slider = document.querySelector(".slide-items");
+const slideItems = document.querySelectorAll(".slide-item");
+const slideDots = document.querySelectorAll(".slide-dot");
 
+/*
+slideItems.forEach((el, i)=> {
+              el.style.transform = `translateX(-${(100)}%)`;
+});
+slideDots.forEach((dot, index)=>{
+  dot.addEventListener('click',()=>{
+    if()
+  })
+});
 
+function toLeft(arr){
+  while (!arr[2].classList.contains('active-slide')){
+    const arr0 = arr[0];
+    for (let i=0; i<arr.length()-1; i++){
+      arr[i] =arr[i+1];
+      arr[arr.length()] = arr0;
+    }
+  };
+}
+*/
+// console.log(slideItems[0])
+
+function toLeft1(arr) {
+  const arrtest = [];
+  const arr0 = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arrtest[i] = arr[i + 1];
+    arrtest[4] = arr0;
+  }
+  return arrtest;
+}
+
+function writeInSlider(arr) {
+  slider.innerHTML = "";
+  arr.forEach((el) => {
+    slider.innerHTML += el.outerHTML;
+  });
+}
+
+console.log(toLeft1(slideItems));
+writeInSlider(slideItems);
+
+// console.log(slideItems[0].outerHTML)
+
+let interval = 0;
+
+// setInterval(() => {
+//   // const arr0 = slideItems[0];
+//   // for (let i=0; i<slideItems.length-1; i++){
+//   //   slideItems[i] =slideItems[i+1];
+//   //   slideItems[slideItems.length-1] = arr0;
+//   // }
+//   slideItems.forEach((el)=> {
+//     el.style.transform = `translateX(${((+el.dataset.slideindex+interval)%4)*100}%)`;
+//   });
+//   interval++;
+//   interval %= 4;
+// }, 200000);
+
+//slider finished
 // HEADER FINISHED
